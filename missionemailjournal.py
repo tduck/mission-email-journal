ae616e0aa2f1db742ef322b5a313cafd39b4e706
Tyler Duckworth <tduckworth91@gmail.com>
2014-04-19 00:06:46 +0000

    Temporary registration page, doesn't do anything yet

--- missionemailjournal.py
+++ missionemailjournal.py
@@ -7,18 +7,10 @@ from flask import render_template
 def register():
 	return render_template('registration.html')
 
-@app.route('/post/<username>')
-def show_user_profile(username):
-	return 'User %s' % username
-
 @app.route('/')
 @app.route('/index')
 def index():
 	return render_template('landing.html')
-
-@app.route('/registration')
-def index():
-	return render_template('registration.html')
 
 if __name__ == '__main__':
 	app.run()
