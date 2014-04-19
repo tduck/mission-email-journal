74ecebbd34fdc5e15c73dafd68bee2abb915baf1
Tyler Duckworth <tduckworth91@gmail.com>
2014-04-19 16:11:01 +0000

    Progress 4/18/14

--- missionemailjournal.py
+++ missionemailjournal.py
@@ -1,8 +1,10 @@
+import sys
 from flask import Flask
-from flask.ext.pymongo import PyMongo
 from flask import render_template
+from flask.ext.mongokit import MongoKit, Document
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 @app.route('/register')
 def register():
