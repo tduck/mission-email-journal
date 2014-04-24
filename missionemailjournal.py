005fa3e3d806e7fbe3ea96b5329d568869cee155
Lance <adamsonlance@gmail.com>
2014-04-23 19:57:42 -0600

    what happened....

--- missionemailjournal.py
+++ missionemailjournal.py
@@ -11,51 +11,51 @@ client = MongoClient('localhost', 27017)
 db = client.myMissionJournal
 
 
-def getDB(self):
-	return MongoClient("localhostls", 27017).myMissionJournal
+# def getDB(self):
+# 	return MongoClient("localhostls", 27017).myMissionJournal
 
-def getUsersColection(self):
-	return self.getDB().users
+# def getUsersColection(self):
+# 	return self.getDB().users
 
-def getMessagesCollection(self):
-	return self.getDB().messages
+# def getMessagesCollection(self):
+# 	return self.getDB().messages
 
-def getUserByEmail(self, email):
-	return self.getUsersColection().find_one({"email": email})
+# def getUserByEmail(self, email):
+# 	return self.getUsersColection().find_one({"email": email})
 
-def isValidUser(self, userName, password):
-	user = self.getUserByEmail(email = userName)
-	if user:
-		hash_pass = hashlib.sha256(password + user["salt"]).hexdigest()
-		if hash_pass == user["password"]:
-			return True
-	return False
+# def isValidUser(self, userName, password):
+# 	user = self.getUserByEmail(email = userName)
+# 	if user:
+# 		hash_pass = hashlib.sha256(password + user["salt"]).hexdigest()
+# 		if hash_pass == user["password"]:
+# 			return True
+# 	return False
 
-def getAllUserMail(self, userName):
-	allUserMessages = self.getMessagesCollection().find({"$or":[{"sender":username},{"recipients":username}]})
-	messageList = []
+# def getAllUserMail(self, userName):
+# 	allUserMessages = self.getMessagesCollection().find({"$or":[{"sender":username},{"recipients":username}]})
+# 	messageList = []
 
-	if allUserMessages:
-		for message in allUserMessages:
-			messageList.append(message)
+# 	if allUserMessages:
+# 		for message in allUserMessages:
+# 			messageList.append(message)
 
-	return messageList
+# 	return messageList
 
-def getUserSentMail(self, userName):
-	allSentMessages = self.getMessagesCollection().find({"sender":username})
-	messageList = []
-	if allSentMessages:
-		for message in allSentMessages:
-			messageList.append(message)
+# def getUserSentMail(self, userName):
+# 	allSentMessages = self.getMessagesCollection().find({"sender":username})
+# 	messageList = []
+# 	if allSentMessages:
+# 		for message in allSentMessages:
+# 			messageList.append(message)
 
-	return messageList
+# 	return messageList
 
-def getUserRecievedMail(self, userName):
-	allInMessages = self.getMessagesCollection().find({"recipients":username})
-	messageList = []
-	if allInMessages:
-		for message in allInMessages:
-			messageList.append(message)
+# def getUserRecievedMail(self, userName):
+# 	allInMessages = self.getMessagesCollection().find({"recipients":username})
+# 	messageList = []
+# 	if allInMessages:
+# 		for message in allInMessages:
+# 			messageList.append(message)
 
 
 
