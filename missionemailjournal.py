1c866f3ee923792591cc052e9d7b30a8637b27b7
Lance <adamsonlance@gmail.com>
2014-04-23 18:33:17 -0600

    moving db file

--- missionemailjournal.py
+++ missionemailjournal.py
@@ -7,58 +7,58 @@ import missionJournalDB
 app = Flask(__name__)
 app.secret_key = os.urandom(256)
 
-class MissionJournalDB:
-
-	def getDB(self):
-		return MongoClient("localhostls", 27017).myMissionJournal
-
-	def getUsersColection(self):
-		return self.getDB().users
-
-	def getMessagesCollection(self):
-		return self.getDB().messages
-
-	def getUserByEmail(self, email):
-		return self.getUsersColection().find_one({"email": email})
-
-	def isValidUser(self, userName, password):
-		user = self.getUserByEmail(email = userName)
-		if user:
-			hash_pass = hashlib.sha256(password + user["salt"]).hexdigest()
-			if hash_pass == user["password"]:
-				return True
-		return False
-
-	def getAllUserMail(self, userName):
-		allUserMessages = self.getMessagesCollection().find({"$or":[{"sender":username},{"recipients":username}]})
-		messageList = []
-
-		if allUserMessages:
-			for message in allUserMessages:
-				messageList.append(message)
-
-		return messageList
-
-	def getUserSentMail(self, userName):
-		allSentMessages = self.getMessagesCollection().find({"sender":username})
-		messageList = []
-		if allSentMessages:
-			for message in allSentMessages:
-				messageList.append(message)
-
-		return messageList
-
-	def getUserRecievedMail(self, userName):
-		allInMessages = self.getMessagesCollection().find({"recipients":username})
-		messageList = []
-		if allInMessages:
-			for message in allInMessages:
-				messageList.append(message)
-
-
 client = MongoClient('localhost', 27017)
 db = client.myMissionJournal
 dbAccess = MissionJournalDB()
+
+
+def getDB(self):
+	return MongoClient("localhostls", 27017).myMissionJournal
+
+def getUsersColection(self):
+	return self.getDB().users
+
+def getMessagesCollection(self):
+	return self.getDB().messages
+
+def getUserByEmail(self, email):
+	return self.getUsersColection().find_one({"email": email})
+
+def isValidUser(self, userName, password):
+	user = self.getUserByEmail(email = userName)
+	if user:
+		hash_pass = hashlib.sha256(password + user["salt"]).hexdigest()
+		if hash_pass == user["password"]:
+			return True
+	return False
+
+def getAllUserMail(self, userName):
+	allUserMessages = self.getMessagesCollection().find({"$or":[{"sender":username},{"recipients":username}]})
+	messageList = []
+
+	if allUserMessages:
+		for message in allUserMessages:
+			messageList.append(message)
+
+	return messageList
+
+def getUserSentMail(self, userName):
+	allSentMessages = self.getMessagesCollection().find({"sender":username})
+	messageList = []
+	if allSentMessages:
+		for message in allSentMessages:
+			messageList.append(message)
+
+	return messageList
+
+def getUserRecievedMail(self, userName):
+	allInMessages = self.getMessagesCollection().find({"recipients":username})
+	messageList = []
+	if allInMessages:
+		for message in allInMessages:
+			messageList.append(message)
+
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -179,14 +179,6 @@ def messages():
 def logout():
 	session.pop('username', None)
 	return redirect('/')
-
-def isValidUser(username, password):
-	user = db.users.find_one({"email": username})
-	if user:
-		hash_pass = hashlib.sha256(password + user["salt"]).hexdigest()
-		if hash_pass == user["password"]:
-			return True
-	return False
 
 
 #this is the REST API
